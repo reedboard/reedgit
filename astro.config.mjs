46d12e0db8237fb597059c14add1772a56fa0c23
reedboard <rwboardman@uiowa.edu>
2026-06-02 19:59:25 -0500

    fix github pages basepath

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://reedboard.github.io',
+	base: '/reedgit'
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
